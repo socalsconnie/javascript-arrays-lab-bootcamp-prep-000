90be143c75858323c902755ba61f1f56a772fba5
alison.theabo@gmail.com <alison.theabo@gmail.com>
2018-04-25 17:54:42 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 const app = "I don't do much."
+
+function destructivelyAppendKitten(name) {
+  kitten.push(name)
+}
+
+function destructivelyPrependKitten(name) {
+  kitten.unshift(name)
+}
+
+function destructivelyRemoveLastKitten() {
+  kitten.pop()
+}
+
+function destructivelyRemoveFirstKitten() {
+  kitten.shift()
+}
+
+function appendKitten(name) {
+  
+}
