654445823be9218841140cbdef4a40035b32d37d
alison.theabo@gmail.com <alison.theabo@gmail.com>
2018-04-25 18:05:59 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ function destructivelyRemoveFirstKitten() {
 }
 
 function appendKitten(name) {
-  var newKittens = kittens.push(name)
+  var newKittens = kittens.push(name);
   return newKittens
 }
 
